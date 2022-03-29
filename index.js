888242ee243647bd4e8181fc3895f55437aee7aa
kyra qiu <kyraqiu@gmail.com>
2022-03-29 14:39:34 -0400

    commit tree 680c36fb61b48135fca19e59059afaa98f721980

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//   ]
+
+// function superbowlWin(record){
+//     for(const element of record){
+//         if (element.result === "W"){
+//             return element.year;
+//         }
+//     }
+// }
+
+function superbowlWin(record){
+    // let win = record.find(elem => elem.result === "W")
+    let theWin = record.find(record => record.result ==="W");
+    if (theWin) {return theWin.year;}
+}
